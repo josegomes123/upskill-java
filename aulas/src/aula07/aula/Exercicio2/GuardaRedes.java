28cfa2fe419e0d11934018637d8fd022cc4b761c
José Gomes <zegomes@gmail.com>
2020-10-28 12:10:27 +0000

    Create GuardaRedes.java

---
+++ aulas/src/aula07/aula/Exercicio2/GuardaRedes.java
@@ -0,0 +1,26 @@
+package aula07.aula.Exercicio2;
+
+public class GuardaRedes extends Jogador {
+    //atributos
+    private int golosSofridos;
+
+    // Constructor, começa com 0 golos sofridos
+    public GuardaRedes(int num, String nome){
+        super(num, nome);
+        this.golosSofridos = 0;
+    }
+
+    //Getters e Setters para golos sofridos
+    public void setGolosSofridos(int golosSofridos) {
+        this.golosSofridos = golosSofridos;
+    }
+
+    public int getGolosSofridos() {
+        return golosSofridos;
+    }
+
+    @Override
+    public String toString() {
+        return super.getNome() + " Numero: " + super.getNum() + " Posiçao: Guarda Redes Golos Sofridos: " + golosSofridos + " Golos Marcados: " + super.getNumGolos();
+    }
+}
