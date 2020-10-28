5e3b6786f1afa1b4355217f1fb2f2aa566b1c354
José Gomes <zegomes@gmail.com>
2020-10-28 12:41:03 +0000

    Create DirectoresRegionais.java

---
+++ aulas/src/aula07/aula/Exercicio1/DirectoresRegionais.java
@@ -0,0 +1,35 @@
+package aula07.aula.Exercicio1;
+
+public class DirectoresRegionais extends Empregados {
+    // Atributos
+    private int lucroMensal;
+
+    // Constructor
+    public DirectoresRegionais(String nome){
+        super(nome);
+    }
+
+    //Getters e Setters
+    public void setLucroMensal(int lucroMensal) {
+        this.lucroMensal = lucroMensal;
+    }
+    public int getLucroMensal() {
+        return lucroMensal;
+    }
+
+    //  Gerente de Loja tem salario variavel conforme cumpre ou nao os objectivos de vendas
+    public void calcularSalario(){
+        // 1% do lucro mensal
+        double percentagemLucro = 0.01 * lucroMensal;
+        //  valor fixo igual ao dobro do dos empregados sem especialização, acrescido
+        //de um prémio que corresponde a 1% do lucro mensal nas lojas da região.
+        setSalario(1600 + percentagemLucro);
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Especializaçao: Director Regional" +
+                " Lucro Mensal: " + lucroMensal;
+    }
+}
